6e5823fc75808c07c88e649adc5d1b42d3d99e05
Thyagoelaborata <108436282+Thyagoelaborata@users.noreply.github.com>
2025-04-17 20:40:50 -0300

    Add files via upload

---
+++ quarta_aula.js
@@ -0,0 +1,17 @@
+function verificationote (note) { 
+                if (note >= 6.0) {
+                    alert("You are approved")
+
+                }else if (note === 5.0){
+                   alert("you are recovery!")
+
+                 }else {
+                    alert("you are donkey")
+                }
+                }
+                let note = Number(prompt("Say your note: "))
+                verificationote(note);
+
+            for(note) {
+
+            }
